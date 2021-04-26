6cfc38c305931040d07bb44f8533a8f3d82b7ce3
d1ll0n <dillonkellar@protonmail.com>
2021-04-25 23:46:53 -0500

    Add description export

--- proposals/12/index.ts
+++ proposals/12/index.ts
@@ -1,1 +1,9 @@
+import fs from 'fs';
+import path from 'path';
+
 export * from './transactions';
+
+export const description = fs.readFileSync(
+  path.join(__dirname, 'description.md'),
+  'utf8'
+);
